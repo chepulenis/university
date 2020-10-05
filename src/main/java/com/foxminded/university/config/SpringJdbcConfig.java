737422bc6c5b1a57c1c76b25d857c86155577ea0
Mykola.Chepulenis <chepulenis@gmail.com>
2020-10-06 00:48:31 +0300

    fixed pom.xml for swagger dependency

--- src/main/java/com/foxminded/university/config/SpringJdbcConfig.java
+++ src/main/java/com/foxminded/university/config/SpringJdbcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -21,14 +22,24 @@ public class SpringJdbcConfig {
     @Value("${spring.datasource.url}")
     String datatSourceUrl;
     
+//  @Bean
+//  public DataSource dataSource() {
+//      JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+//      DataSource dataSource = dataSourceLookup.getDataSource(datatSourceUrl);
+//      return dataSource;
+//  }
+  
   @Bean
-  public DataSource dataSource() {
-      JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
-      DataSource dataSource = dataSourceLookup.getDataSource(datatSourceUrl);
-      return dataSource;
+  DataSource dataSource() {
+      DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+      driverManagerDataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/university");
+      driverManagerDataSource.setUsername("postgres");
+      driverManagerDataSource.setPassword("qwerty123");
+      driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+      return driverManagerDataSource;
   }
   
-     @Bean
+    @Bean
     public JdbcTemplate jdbcTemplate() throws NamingException {
         var template = new JdbcTemplate();
         template.setDataSource(dataSource());
